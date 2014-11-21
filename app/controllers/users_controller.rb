39de5522bbe4c3e02fa4249d02c312df6fae17e2
Alex Ung <alexung.ucsb@gmail.com>
2014-11-21 18:38:47 -0500

    Login works, but doesn't redirect them to homepage with new navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,29 @@
 class UsersController < ApplicationController
 
   def create
-    @user = User.new(params[:user])
-    @user.password = params[:password]
+    @user = User.new
+    @user.username = params[:users][:username]
+    @user.password = params[:users][:password]
     @user.save!
-  end
 
-  def login
-    @user = User.find_by(username: params[:username])
-    if @user.password == params[:password]
-      give_token
+    if @user.password == params[:users][:password]
+      #give_token
       redirect_to root_path
     else
       flash[:error] = "Your user information was not found"
       redirect_to users_path
     end
   end
+
+  # def login
+  #   @user = User.find_by(username: params[:username])
+  #   if @user.password == params[:password]
+  #     give_token
+  #     redirect_to root_path
+  #   else
+  #     flash[:error] = "Your user information was not found"
+  #     redirect_to users_path
+  #   end
+  # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 50 }
   # users.password_hash in the database is a :string
   has_secure_password
 end
